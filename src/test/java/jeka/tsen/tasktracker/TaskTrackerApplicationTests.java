b4b48c4bd5e2e554329d5fd86bd329a76641cf90
Jeka <y.kukhar21@gmail.com>
2025-11-09 15:50:49 +0200

    first commit

---
+++ src/test/java/jeka/tsen/tasktracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package jeka.tsen.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
